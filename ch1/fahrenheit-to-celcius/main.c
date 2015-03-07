40056a8513313d055ba07c62c8d70091100c8820
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-07 15:37:53 -0500

    Add constants for start, end & step.

--- ch1/fahrenheit-to-celcius/main.c
+++ ch1/fahrenheit-to-celcius/main.c
@@ -7,12 +7,16 @@ Print a table with fahrenheit values and corresponding celcius values.
 #include "temperature_table.h"
 
 int main(){
+	const temperature_t START = 0;
+	const temperature_t END = 200;
+	const temperature_t STEP = 20;
+
 	table_t temperatureTable;
 	temperatureTable = makeTable( (const char *[]){"Fahrenheit", "Celcius"}, 2, "\t" );
 
 	printHeaders(temperatureTable);
 
-	for (double fahrenheit=0; fahrenheit<=200; fahrenheit += 20){
+	for (double fahrenheit=START; fahrenheit<=END; fahrenheit += STEP){
 		printRow(fahrenheit, toCelcius(fahrenheit), temperatureTable);
 	}
 
